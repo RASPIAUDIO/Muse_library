8536c61801123702d01d88de1d5a372dd81f2dc9
RASPIAUDIO <RASPIAUDIO@github.com>
2024-11-12 17:34:18 +0100

    12/11/24

--- examples/LUXE/record2/record2.ino
+++ examples/LUXE/record2/record2.ino
@@ -19,7 +19,7 @@ File f;
 // Initialize your objects and variables
 I2SClass i2s;
 ES8388 es;
-pcm_wav_header_t H = PCM_WAV_HEADER_DEFAULT(0, 16, 16000, 1);
+pcm_wav_header_t H = PCM_WAV_HEADER_DEFAULT(0, 16, 44100, 2);
 
 
 void setup() {
@@ -48,10 +48,21 @@ void setup() {
 //////I2S init
   i2s.setPins(I2S_BCLK, I2S_LRCK, I2S_SDOUT, I2S_SDIN, I2S_MCLK);
 
-  if (!i2s.begin(I2S_MODE_STD, 16000, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO, I2S_STD_SLOT_LEFT)) {
+  if (!i2s.begin(I2S_MODE_STD, 44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO, I2S_STD_SLOT_LEFT)) {
     Serial.println("Failed to initialize I2S!");
     while (1); // do nothing
   }
+
+  if (!i2s.configureRX(44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO, I2S_RX_TRANSFORM_NONE) ){
+    Serial.println("Failed to initialize RX Channel!");
+    while (1); // do nothing
+  }
+
+ if (!i2s.configureTX(44100, I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO)) {
+    Serial.println("Failed to initialize TX Channel!");
+    while (1); // do nothing
+  }
+   
 // codec ES8388 init
   Serial.printf("Connect to ES8388 codec... ");
   while (not es.begin(IIC_DATA, IIC_CLK))
@@ -74,7 +85,8 @@ void setup() {
 void loop() {
 #define T 1024
     size_t t; 
-    char buf[T];
+    char buf[T/2];
+    char bufb[T];
     int tb;
     int L;
     if (gpio_get_level(BUTTON_PAUSE) == 0)
@@ -85,16 +97,28 @@ void loop() {
     
     printf("recording...\n");
     L = 0;
+    uint8_t M, N;
     while(gpio_get_level(BUTTON_PAUSE) == 1)
     {
-      t = i2s.readBytes(buf, T);
-      f.write((const uint8_t*)buf, (int) t);
-      L += t;
+     t = i2s.readBytes(buf, T/2);
+ // one channel ==> two channels    
+     for(int i=0;i<t;i=i+2)
+     {
+      bufb[i+i] = buf[i];
+      bufb[i+i+1] = buf[i+1];
+      bufb[i+i+2] = buf[i];
+      bufb[i+i+3] = buf[i+1];     
+     }   
+     f.write((const uint8_t*)bufb, t+t);    
+     L = L + 2*t;   
     }
-    // write the header
+
     f.seek(0);
+ // to refresh size values   
     H.descriptor_chunk.chunk_size = L + PCM_WAV_HEADER_SIZE ;
     H.data_chunk.subchunk_size = L;
+    
+ // write the header   
     f.write((uint8_t*)&H, PCM_WAV_HEADER_SIZE);
     
     f.seek(L + PCM_WAV_HEADER_SIZE);
@@ -107,8 +131,8 @@ void loop() {
     f.seek(PCM_WAV_HEADER_SIZE);    // skip the header
     do
     {
-     tb = f.read((uint8_t*)buf, T);
-     i2s.write((uint8_t*)buf, (size_t)tb);
+     tb = f.read((uint8_t*)bufb, T);
+     i2s.write((uint8_t*)bufb, (size_t)tb);
     }while(tb > 0);
     f.close();
   }
